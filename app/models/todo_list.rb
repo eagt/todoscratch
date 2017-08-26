aadb6cb08b24501aec2dd670329cf93c991190c2
Antonio Garcia <egartru@gmail.com>
2017-08-26 06:12:20 +0100

    Validatind name on TodoList and Tasks

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,7 +1,9 @@
 class TodoList < ApplicationRecord
 
+ validates_presence_of :name
+ 
  has_many :tasks, dependent: :destroy
  accepts_nested_attributes_for :tasks, allow_destroy: true,
- 							   reject_if: proc { |att| att ['description'].blank?}
+ 							   reject_if: proc { |att| att ['name'].blank?}
 
 end
